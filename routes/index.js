a6d9c595855c471363f0218a9bf4ca474aa8c345
Dmitry.Kozhevnikov <d.o.kozhevnikov@gmail.com>
2017-02-12 15:06:30 +0700

    make pages render data from mongo

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,24 @@
  * Routing
  */
 module.exports = function(app){
-  "use strict";
+    "use strict";
 
-    // main page - top by karma
+    var data = require("../data");
+
+    // main page - all persons
     app.get('/', function(req, res){
-        res.render('index', { title: 'Astro Karma'})
+        data.getPersons(function (err, persons) {
+            if(err) throw err;
+            console.log(persons);
+            res.render('index', { title: 'Astro Karma', persons: persons})
+        });
     });
 
     // person page
-    app.get('/person', function(req, res){
-        res.render('person', { title: 'Astro Karma'})
+    app.get('/person/:id', function(req, res){
+        data.getPerson(req.params.id, function (err, person) {
+            if(err) throw err;
+            res.render('person', { title: 'Astro Karma', person: person})
+        });
     });
 }
